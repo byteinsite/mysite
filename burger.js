1244ed31410390f19978c36162c7cf48224e4f18
byteinsite <66483013+byteinsite@users.noreply.github.com>
2021-10-07 20:17:43 +0300

    Update burger.js

--- burger.js
+++ burger.js
@@ -2,13 +2,4 @@
 document.querySelector('.burger').onclick = function(){
     document.querySelectorAll('.burger-icon, .header__menu2').forEach(elem => elem.classList.toggle('active'));
     document.querySelector('body').classList.toggle('lock');
-    // for (var elem of menu){elem.classList.toggle('active');
 }
-// document.querySelector('.burger').onclick = function(){
-//     document.querySelector('.burger-icon').classList.toggle('active');
-// }
-// $(document).ready(function() {
-// 	$('.burger').click(function(event){
-// 		$('.burger,.header__menu').toggleClass('active');
-// 	});
-// });
